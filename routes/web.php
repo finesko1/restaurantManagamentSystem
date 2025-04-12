408d8034905b8223c3a6beb1a108b6f5c2fdb426
Mikhail <mikhail.fedorov.044@mail.ru>
2025-04-12 20:30:05 +0300

    init routes for waiter and cook

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\CookController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderItemController;
 use App\Http\Controllers\WaiterController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -10,9 +12,6 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/waiter/home', [WaiterController::class, 'index'])->middleware('auth:waiter')->name('waiter.home');
-Route::get('/cook/home', [CookController::class, 'index'])->middleware('auth:cook')->name('cook.home');
-
 Route::group(['middleware' => 'guest'], function () {
     Auth::routes();
 });
@@ -20,3 +19,46 @@ Route::group(['middleware' => 'guest'], function () {
 Route::post('/{role}/logout', [LoginController::class, 'logout'])
     ->name('logout')
     ->middleware('auth:{role}');
+
+
+// Ресурсы для официанта
+Route::group(['middleware' => 'auth:waiter'], function () {
+    // Представление заказов
+    Route::get('/waiter/home', [WaiterController::class, 'index'])
+        ->name('waiter.index');
+
+    // Управление заказами
+    Route::get('/order', [OrderController::class, 'index'])
+        ->name('order.index');
+    Route::post('/order', [OrderController::class, 'store'])
+        ->name('order.store');
+    Route::get('/order/{id}', [OrderController::class, 'show'])
+        ->name('order.show');
+    Route::delete('/order/{id}', [OrderController::class, 'destroy'])
+        ->name('order.destroy');
+
+    // Управление блюдами
+    Route::get('/order/items', [OrderItemController::class, 'index'])
+        ->name('order.items.index');
+    Route::post('/order/items', [OrderItemController::class, 'store'])
+        ->name('order.items.store');
+    Route::get('/order/items/{id}', [OrderItemController::class, 'show'])
+        ->name('order.items.show');
+    Route::delete('/order/items/{id}', [OrderItemController::class, 'destroy'])
+        ->name('order.items.destroy');
+});
+
+// Ресурсы для повара
+Route::group(['middleware' => 'auth:cook'], function () {
+    // Представление заказов
+    Route::get('/cook/home', [CookController::class, 'index'])
+        ->name('cook.index');
+
+    // Управление заказами
+    Route::get('/order', [OrderController::class, 'index'])
+        ->name('order.index');
+    Route::get('/order/{id}', [OrderController::class, 'show'])
+        ->name('order.show');
+    Route::patch('/order/{id}/status', [OrderController::class, 'updateStatus'])
+        ->name('order.status.update');
+});
