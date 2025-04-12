ebb5fa834ee18d692df82c877f31302de5ce7e28
Mikhail <mikhail.fedorov.044@mail.ru>
2025-04-12 18:03:57 +0300

    add some fixes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,21 +3,12 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
     public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
     {
         //
     }
