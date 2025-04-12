fcb7425ee89b0bfa8a8093155d9c36cdb268eb90
Mikhail <mikhail.fedorov.044@mail.ru>
2025-04-12 18:13:35 +0300

    added init routes for authorization

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\CookController;
+use App\Http\Controllers\WaiterController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/waiter/home', [WaiterController::class, 'index'])->middleware('auth:waiter')->name('waiter.home');
+Route::get('/cook/home', [CookController::class, 'index'])->middleware('auth:cook')->name('cook.home');
+
+Route::group(['middleware' => 'guest'], function () {
+    Auth::routes();
+});
+
+Route::post('/{role}/logout', [LoginController::class, 'logout'])
+    ->name('logout')
+    ->middleware('auth:{role}');
