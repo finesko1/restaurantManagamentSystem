4a508ed4733a65fb3bbce6f327c45c01a3702f4b
Mikhail <mikhail.fedorov.044@mail.ru>
2025-04-14 03:12:48 +0300

    init auth guarders

--- app/Policies/RolePolicy.php
+++ app/Policies/RolePolicy.php
@@ -3,16 +3,17 @@
 namespace App\Policies;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class RolePolicy
 {
     public function accessWaiterHome(User $user)
     {
-        return $user->role === 'waiter';
+        return Auth::guard('waiter')->check();
     }
 
     public function accessCookHome(User $user)
     {
-        return $user->role === 'cook';
+        return Auth::guard('cook')->check();
     }
 }
