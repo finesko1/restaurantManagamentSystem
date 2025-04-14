043ea34f70d1e6b9da9b07a9511e8ced63f40515
Mikhail <mikhail.fedorov.044@mail.ru>
2025-04-14 03:44:13 +0300

    starting point

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,91 +9,11 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <ul class="navbar-nav ms-auto">
-                        @if (Auth::guard('cook')->check())
-                            <li class="mx-2">Добро пожаловать, {{ Auth::guard('cook')->user()->name }}!</li>
-                            <li>
-                                <a class="dropdown-item" href="#"
-                                   onclick="event.preventDefault(); document.getElementById('cook-logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-                                <form id="cook-logout-form" action="{{ route('cook.logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </li>
-                        @elseif (Auth::guard('waiter')->check())
-                            <li class="mx-2">Добро пожаловать, {{ Auth::guard('waiter')->user()->name }}!</li>
-                            <li>
-                                <a class="dropdown-item" href="#"
-                                   onclick="event.preventDefault(); document.getElementById('waiter-logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-                                <form id="waiter-logout-form" action="{{ route('waiter.logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </li>
-                        @else
-                            @guest
-                                @if (Route::has('login'))
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                    </li>
-                                @endif
-
-                                @if (Route::has('register'))
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                    </li>
-                                @endif
-                            @else
-                                <li class="nav-item dropdown">
-                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                        {{ Auth::user()->name }}
-                                    </a>
-
-                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="#"
-                                           onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                            @csrf
-                                        </form>
-                                    </div>
-                                </li>
-                            @endguest
-                        @endif
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+<div id="app">
+</div>
 </body>
 </html>
