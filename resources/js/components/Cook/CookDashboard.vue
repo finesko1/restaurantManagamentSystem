6821203c323f430ec08169ecc77966e2dd605484
Mikhail <mikhail.fedorov.044@mail.ru>
2025-04-14 03:25:39 +0300

    created form for cook role

---
+++ resources/js/components/Cook/CookDashboard.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="cook-dashboard">
+        <OrderList>
+        </OrderList>
+    </div>
+</template>
+
+<script>
+import { useStore } from 'vuex';
+import { computed } from 'vue';
+import OrderList from "@/components/Waiter/OrderList.vue";
+
+export default {
+    name: 'CookDashboard',
+    components: {OrderList},
+    setup() {
+        const store = useStore();
+        const role = computed(() => store.getters['auth/role']);
+
+        return {
+            role
+        };
+    },
+    beforeRouteEnter(to, from, next) {
+        const store = useStore();
+        if (store.getters['auth/role'] !== 'cook') {
+            next('/');
+        } else {
+            next();
+        }
+    }
+};
+</script>
