7339a3e81381e975782975aeaacb93d9236423f6
Mikhail <mikhail.fedorov.044@mail.ru>
2025-04-14 03:37:33 +0300

    init singles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\OrderItem;
+use App\Services\MenuService;
+use App\Services\OrderItemService;
+use App\Services\OrderService;
 use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
@@ -10,6 +14,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Регистрация MenuService
+        $this->app->singleton(MenuService::class, function () {
+            return new MenuService();
+        });
+        // Регистрация OrderService
+        $this->app->singleton(OrderService::class, function () {
+            return new OrderService();
+        });
+        // Регистрация OrderItemService
+        $this->app->singleton(OrderItemService::class, function () {
+            return new OrderItemService();
+        });
     }
 }
